6ce01dc728f943ee3f99d031d3cbb73e45bc01cb
llongaray <llongaray77@gmail.com>
2024-03-05 17:08:35 -0300

    galeria

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
+    print("Starting development server at http://192.168.18.102:8000/")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
