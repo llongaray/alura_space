0b44c99faf5e0d63979aae71094f194277002fc9
llongaray <llongaray77@gmail.com>
2024-03-06 12:03:12 -0300

    add

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from galeria.views import index
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',  index),  # Remova a barra inicial aqui
+    path('',  include('galeria.urls')),  # Remova a barra inicial aqui
 ]
