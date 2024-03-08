36f3ef3bf29a270b5bcb615969774ff8b21f5ac4
llongaray <llongaray77@gmail.com>
2024-03-08 17:54:16 -0300

    add12

--- setup/settings.py
+++ setup/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.18.102','192.168.15.115','0.0.0.0','127.0.0.1','localhost']
 
 
 # Application definition
