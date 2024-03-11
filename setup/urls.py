3021e5f0382209e40b57ed848e91de25ec2d4715
llongaray <llongaray77@gmail.com>
2024-03-11 16:16:35 -0300

    +1

--- setup/urls.py
+++ setup/urls.py
@@ -7,4 +7,5 @@ from galeria.admin import meu_admin_site  # Importe o MeuAdminSite aqui
 urlpatterns = [
     path('admin/', meu_admin_site.urls),  # Use o urls do MeuAdminSite
     path('', include('galeria.urls')),
+    path('', include('usuarios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ usuarios/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(label="Nome de Login", required=True, max_length=100, widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Ex.: Jaojaja.700"}))
+    senha = forms.CharField(label="Senha", required=True, max_length=70, widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder": "Digite sua senha"}))
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(label="Nome de cadastro", required=True, max_length=100, widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Ex.: Jaojaja.700"}))
+    email_cadastro = forms.EmailField(label="E-mail", required=True, max_length=100, widget=forms.EmailInput(attrs={"class":"form-control", "placeholder": "Ex.: Jaojaja.700@gmail.com"}))
+    senha_cadastro = forms.CharField(label="Senha", required=True, max_length=70, widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder": "Digite sua senha"}))
+    resenha_cadastro = forms.CharField(label="Confirme sua Senha", required=True, max_length=70, widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder": "Digite sua senha novamente"}))
